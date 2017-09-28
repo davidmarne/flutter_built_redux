b2e85f32af3dd90192407412a24e0d365ecb2c29
David Marne <davemarne@gmail.com>
2017-09-27 23:48:50 -0600

    update readme

--- example/chat_app/lib/models.dart
+++ example/chat_app/lib/models.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 class ChatUser {
   ChatUser()
       : name = "Guest${new Random().nextInt(1000)}",
-        color = Colors.accents[new Random().nextInt(Colors.accents.length)][700];
+        color =
+            Colors.accents[new Random().nextInt(Colors.accents.length)][700];
 
   final String name;
   final Color color;
